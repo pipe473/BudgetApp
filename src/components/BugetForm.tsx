64a7476e65cc7756c1ea741e7ba09e747869c818
pipe473 <bedoyafelipe743@gmail.com>
2024-07-24 14:33:27 +0200

    Inicio de proyecto

---
+++ src/components/BugetForm.tsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+
+export default function BugetForm() {
+
+    const [budget, setBudget] = useState(0)
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setBudget(e.target.valueAsNumber)  
+    }
+
+    return (
+        <form className='space-y-5'>
+            <div className='flex flex-col space-y-5'>
+                <label htmlFor="budget" className='text-4xl text-blue-600 font-bold text-center'>
+                    Definir Presupuesto
+                </label>
+            </div>
+            <input
+                id='budgetID'
+                type="number"
+                className='w-full bg-white border-gray-200 p-2'
+                placeholder='Define tu presupuesto'
+                name='budget'
+                value={budget}
+                onChange={handleChange}
+            />
+            <input
+                type="submit"
+                value={'Definir Presupuesto'}
+                className='bg-blue-600 hover:bg-blue-700 w-full p-2 text-white uppercase font-black cursor-pointer'
+            />
+        </form>
+    )
+}
