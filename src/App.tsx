a406f3fdf368593fd3c899d6cd1e780c67dde53c
pipe473 <bedoyafelipe743@gmail.com>
2024-07-26 12:18:56 +0200

    Creando componente que se muestre condicionalmente cuando se añada una cantidad y su grafica

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { useMemo } from 'react'
 import BudgetForm from './components/BugetForm'
+import { useBudget } from './hooks/useBudget'
+import BudgetTracker from './components/BudgetTracker'
 
 function App() {
+
+  const  { state } = useBudget()
+ 
+const isValidBudget = useMemo(() => state.budget > 0, [state.budget])
+  
 
   return (
     <>
@@ -11,7 +19,7 @@ function App() {
       </header>
 
       <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
-        <BudgetForm />
+          { isValidBudget ? <BudgetTracker /> : <BudgetForm />}
       </div>
     </>
   )
---
+++ src/helpers/index.ts
@@ -0,0 +1,3 @@
+export function formatCurrency(amount: number) {
+    return new Intl.NumberFormat('es-ES', { style: 'currency', currency: 'EUR' }).format(amount)
+}
