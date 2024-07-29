5cee4ec1b5d85e7a11ea2a413400421a76923ac3
pipe473 <bedoyafelipe743@gmail.com>
2024-07-29 11:43:18 +0200

    Mostrando alerta si hay campos sin rellenar y falla el formulario

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -4,6 +4,7 @@ import { categories } from "../data/categories"
 import DatePicker from 'react-date-picker'
 import 'react-calendar/dist/Calendar.css'
 import 'react-date-picker/dist/DatePicker.css'
+import ErrorMessage from "./ErrorMessage"
 
 export default function ExpenseForm() {
     const [expense, setExpense] = useState<DraftExpense>({
@@ -12,6 +13,8 @@ export default function ExpenseForm() {
         category: '',
         date: new Date()
     })
+
+    const [error, setError] = useState('')
 
     const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
         const { name, value } = e.target
@@ -29,11 +32,25 @@ export default function ExpenseForm() {
         })
     }
 
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        // Validar
+        if (Object.values(expense).includes('')) {
+            setError('Todos los campos son obligatorios')
+            return
+        }
+        console.log('todo bien...');
+    }
+
     return (
-        <form className="space-y-5">
+        <form className="space-y-5" onSubmit={handleSubmit}>
             <legend className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2">
                 Nuevo Gasto
             </legend>
+
+            {error && <ErrorMessage>{error}</ErrorMessage>}
+
             <div className="flex flex-col gap-2">
                 <label htmlFor="expenseName">
                     Nombre Gasto:
