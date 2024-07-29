408cf4310f2620ad116022d2eecb37241d4f0553
pipe473 <bedoyafelipe743@gmail.com>
2024-07-29 09:09:59 +0200

    Añadiendo calendario a formulario de gastos

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,4 +1,11 @@
-import { categories } from "../data/categories";
+import { categories } from "../data/categories"
+import DatePicker from 'react-date-picker'
+import 'react-calendar/dist/Calendar.css'
+import 'react-date-picker/dist/DatePicker.css'
+
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 export default function ExpenseForm() {
     return (
@@ -49,6 +56,16 @@ export default function ExpenseForm() {
                     ))}
                 </select>
             </div>
+            
+            <div className="flex flex-col gap-2">
+                <label htmlFor="amount">
+                    Fecha Gasto:
+                </label>
+                <DatePicker
+                   className="bg-slate-100 p-2 border-0" 
+                />
+            </div>
+
             <input 
                 type="submit"
                 className="bg-blue-600 cursor-pointer w-full p-2 text-white uppercase font-bold rounded-lg"
