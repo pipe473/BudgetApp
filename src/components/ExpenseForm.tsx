5b486dfe1a9fa30abf0564366643e89256733811
pipe473 <bedoyafelipe743@gmail.com>
2024-07-30 14:37:57 +0200

    Mostrar de forma condicional si es un gasto nuevo o edicion

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -68,7 +68,7 @@ export default function ExpenseForm() {
     return (
         <form className="space-y-5" onSubmit={handleSubmit}>
             <legend className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2">
-                Nuevo Gasto
+                {state.editingId ? 'Guardar Cambios' : 'Nuevo Gasto'}
             </legend>
 
             {error && <ErrorMessage>{error}</ErrorMessage>}
@@ -135,7 +135,7 @@ export default function ExpenseForm() {
             <input
                 type="submit"
                 className="bg-blue-600 cursor-pointer w-full p-2 text-white uppercase font-bold rounded-lg"
-                value={'Registrar Gasto'}
+                value={state.editingId ? 'Guardar Cambios' :'Registrar Gasto'}
             />
         </form>
     )
