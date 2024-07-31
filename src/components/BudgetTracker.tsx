997a8a2bd113491fd1f944607699bf077a2c7ae4
pipe473 <bedoyafelipe743@gmail.com>
2024-07-31 09:20:44 +0200

    Mostrando la cantidad que hay de presupuesto, gastado y disponible

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,32 +1,40 @@
+import { useMemo } from "react";
+import { useBudget } from "../hooks/useBudget";
 import AmountDisplay from "./AmountDisplay";
 
 export default function BudgetTracker() {
-  return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
-        <div className="flex justify-center">
-            <img src="/grafico.jpg" alt="Gráfica de gastos" />
-        </div>
-        <div className="flex flex-col justify-center items-center gap-8">
-            <button
-                type="button"
-                className="bg-pink-600 w-full p-2 text-white uppercase font-bold rounded-lg"
-            >
-                Resetear App
-            </button>
+    const { state } = useBudget()
 
-            <AmountDisplay 
-                label="Presupuesto"
-                amount={300}
-            />
-            <AmountDisplay 
-                label="Disponible"
-                amount={200}
-            />
-            <AmountDisplay 
-                label="Gastado"
-                amount={100}
-            />
+    const totalExpenses = useMemo(() => state.expenses.reduce((total, expense) => expense.amount + total, 0), [state.expenses])
+    const remainingBudget = state.budget - totalExpenses
+
+
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+            <div className="flex justify-center">
+                <img src="/grafico.jpg" alt="Gráfica de gastos" />
+            </div>
+            <div className="flex flex-col justify-center items-center gap-8">
+                <button
+                    type="button"
+                    className="bg-pink-600 w-full p-2 text-white uppercase font-bold rounded-lg"
+                >
+                    Resetear App
+                </button>
+
+                <AmountDisplay
+                    label="Presupuesto"
+                    amount={state.budget}
+                />
+                <AmountDisplay
+                    label="Disponible"
+                    amount={remainingBudget}
+                />
+                <AmountDisplay
+                    label="Gastado"
+                    amount={totalExpenses}
+                />
+            </div>
         </div>
-    </div>
-  )
+    )
 }
