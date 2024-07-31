44226917ea4553098a06f8a0811b1b26c26ca200
pipe473 <bedoyafelipe743@gmail.com>
2024-07-31 15:13:40 +0200

    Escribiendo en el state la categoria a filtrar

--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -1,5 +1,5 @@
 import { v4 as uuidv4 } from 'uuid'
-import { DraftExpense, Expense } from "../types"
+import { Category, DraftExpense, Expense } from "../types"
 
 export type BudgetActions =
     { type: 'add-budget', payload: { budget: number } } |
@@ -9,13 +9,15 @@ export type BudgetActions =
     { type: 'remove-expense', payload: { id: Expense['id'] } } |
     { type: 'get-expense-by-id', payload: { id: Expense['id'] } } |
     { type: 'update-expense', payload: { expense: Expense } } |
-    { type: 'reset-app' }
+    { type: 'reset-app' } |
+    { type: 'add-filter-category', payload: { id: Category['id'] } }
 
 export type BudgetState = {
     budget: number
     modal: boolean
     expenses: Expense[]
     editingId: Expense['id']
+    currentCategory: Category['id']
 }
 
 const createExpense = (draftExpense: DraftExpense): Expense => {
@@ -39,7 +41,8 @@ export const initialState: BudgetState = {
     budget: initialBudget(),
     modal: false,
     expenses: localStorageExpenses(),
-    editingId: ''
+    editingId: '',
+    currentCategory: ''
 }
 
 export const budgetReducer = (
@@ -104,6 +107,13 @@ export const budgetReducer = (
             ...state,
             budget: 0,
             expenses: [],
+        }
+    }
+
+    if (action.type === 'add-filter-category') {
+        return {
+            ...state,
+            currentCategory: action.payload.id
         }
     }
 
