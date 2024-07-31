ae71d3fecd43b226fcd5571c4fb956085dab02d2
pipe473 <bedoyafelipe743@gmail.com>
2024-07-31 10:27:14 +0200

    Evitar que sobrepasen los gastos el presupuesto

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -16,12 +16,14 @@ export default function ExpenseForm() {
     })
 
     const [error, setError] = useState('')
-    const { dispatch, state } = useBudget()
+    const [previousAmount, setPreviousAmount] = useState(0)
+    const { dispatch, state, remainingBudget, } = useBudget()
 
     useEffect(() => {
         if (state.editingId) {
             const editingExpense = state.expenses.filter(currentExpense => currentExpense.id === state.editingId)[0]
             setExpense(editingExpense)
+            setPreviousAmount(editingExpense.amount)
         }
     }, [state.editingId])
 
@@ -49,6 +51,13 @@ export default function ExpenseForm() {
             setError('Todos los campos son obligatorios')
             return
         }
+
+        // Validar que no me pase del límite
+        if ((expense.amount - previousAmount) > remainingBudget ) {
+            setError('Esta cantidad supera el presupuesto')
+            return
+        }
+
         // Agregar o actualizar el gasto
         if (state.editingId) {
             dispatch({ type: 'update-expense', payload: { expense: { id: state.editingId, ...expense } } })
@@ -63,6 +72,7 @@ export default function ExpenseForm() {
             category: '',
             date: new Date()
         })
+        setPreviousAmount(0)
     }
 
     return (
